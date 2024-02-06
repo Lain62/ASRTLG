5acaf577368c74cc04d8b9a4fc283896dae9b7ea
lain62 <laimadelain@gmail.com>
2024-02-06 21:21:32 +0700

    added captain

--- script.js
+++ script.js
@@ -14,6 +14,7 @@ const acridUtility2 = () => utility('./assets/acrid/acridUtility2.webp', 'Frenzi
 const acridSpecial = () => special('./assets/acrid/acridSpecial.webp', 'Epidemic')
 
 function acridGenerate() {
+    removeChildAbility()
     acridProfile()
     acridPrimary()
     acridSpecial()
@@ -53,6 +54,7 @@ const artificerSpecial = () => special('./assets/artificer/artificerSpecial.webp
 const artificerSpecial2 = () => special('./assets/artificer/artificerSpecial2.webp', 'Ion Surge')
 
 function artificerGenerate() {
+    removeChildAbility()
     artificerProfile()
     artificerPassive()
     artificerUtility()
@@ -92,6 +94,7 @@ const banditSpecial = () => special('./assets/bandit/banditSpecial.webp', 'Light
 const banditSpecial2 = () => special('./assets/bandit/banditSpecial2.webp', 'Desperado')
 
 function banditGenerate() {
+    removeChildAbility()
     banditProfile()
     banditPassive()
     banditUtility()
@@ -113,6 +116,90 @@ function banditGenerate() {
     } else {
         banditSpecial2()
     }
+}
+
+// Captain
+const captain = document.getElementById('captain')
+
+captain.addEventListener('click', captainGenerate)
+
+const captainProfile = () => profile('./assets/captain/captainProfile.webp', 'Captain')
+const captainPassive = () => passive('./assets/captain/captainPassive.webp', 'Defensive Microbots')
+const captainPrimary = () => primary('./assets/captain/captainPrimary.webp', 'Vulcan Shotgun')
+const captainSecondary = () => secondary('./assets/captain/captainSecondary.webp', 'Power Tazer')
+const captainUtility = () => utility('./assets/captain/captainUtility.webp', 'Orbital Probe')
+const captainUtility2 = () => utility('./assets/captain/captainUtility2.webp', 'OGM-72 \'DIABLO\' Strike')
+const captainSpecial = () => special('./assets/captain/captainSpecial.webp', 'Orbital Supply Beacon')
+
+function captainGenerate() {
+    removeChildAbility()
+    captainProfile()
+    captainPassive()
+    captainPrimary()
+    captainSecondary()
+    captainSpecial()
+    let utility = Math.floor(Math.random() * 2)
+    let specialChild = Math.floor(Math.random() * 4)
+    let specialChild2 = Math.floor(Math.random() * 4)
+    if (utility == 0) {
+        captainUtility()
+    } else {
+        captainUtility2()
+    }
+    const divAbilites = document.getElementById('cont-char-abilites')
+
+    const divAbilityChild = document.createElement('div')
+    const divAbilityChild2 = document.createElement('div')
+    const imageSpecialChild = document.createElement('img')
+    const textSpecialChild = document.createElement('p')
+
+    imageSpecialChild.className = 'img-ability-child'
+    textSpecialChild.className = 'label-ability-child'
+
+    if (specialChild == 0) {
+        imageSpecialChild.src = './assets/captain/captainSpecialChild.webp'
+        textSpecialChild.innerText = 'Beacon: Healing'
+    } else if (specialChild == 1) {
+        imageSpecialChild.src = './assets/captain/captainSpecialChild2.webp'
+        textSpecialChild.innerText = 'Beacon: Shocking'
+    } else if (specialChild == 2) {
+        imageSpecialChild.src = './assets/captain/captainSpecialChild3.webp'
+        textSpecialChild.innerText = 'Beacon: Resupply'
+    } else if (specialChild == 3) {
+        imageSpecialChild.src = './assets/captain/captainSpecialChild4.webp'
+        textSpecialChild.innerText = 'Beacon: Hacking'
+    }
+
+    const imageSpecialChild2 = document.createElement('img')
+    const textSpecialChild2 = document.createElement('p')
+
+    imageSpecialChild2.className = 'img-ability-child'
+    textSpecialChild2.className = 'label-ability-child'
+
+    if (specialChild2 == 0) {
+        imageSpecialChild2.src = './assets/captain/captainSpecialChild.webp'
+        textSpecialChild2.innerText = 'Beacon: Healing'
+    } else if (specialChild2 == 1) {
+        imageSpecialChild2.src = './assets/captain/captainSpecialChild2.webp'
+        textSpecialChild2.innerText = 'Beacon: Shocking'
+    } else if (specialChild2 == 2) {
+        imageSpecialChild2.src = './assets/captain/captainSpecialChild3.webp'
+        textSpecialChild2.innerText = 'Beacon: Resupply'
+    } else if (specialChild2 == 3) {
+        imageSpecialChild2.src = './assets/captain/captainSpecialChild4.webp'
+        textSpecialChild2.innerText = 'Beacon: Hacking'
+    }
+
+    divAbilityChild.className = 'ability-child'
+    divAbilityChild2.className = 'ability-child'
+
+    divAbilityChild.appendChild(imageSpecialChild)
+    divAbilityChild.appendChild(textSpecialChild)
+    divAbilityChild2.appendChild(imageSpecialChild2)
+    divAbilityChild2.appendChild(textSpecialChild2)
+
+    divAbilites.appendChild(divAbilityChild)
+    divAbilites.appendChild(divAbilityChild2)
 }
 
 // variables for easy element creation
@@ -162,4 +249,11 @@ const special = (img, label) => {
 
     image.src = img
     text.innerText = label
+}
+
+const removeChildAbility = () => {
+    const elements = document.getElementsByClassName('ability-child');
+    while(elements.length > 0){
+        elements[0].parentNode.removeChild(elements[0]);
+    }
 }
